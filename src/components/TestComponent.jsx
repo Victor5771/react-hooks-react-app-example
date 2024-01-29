aa1276d0fea2e939b7913b9390debe5dbf9b8a80
victor kyalo <victor.kyalo@student.moringaschool.com>
2024-01-30 00:49:05 +0300

    A React App Example-Done

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,12 @@
+// TestComponent.js
+import React from 'react';
+
+function TestComponent() {
+  return (
+    <div>
+      {/* Your TestComponent content goes here */}
+    </div>
+  );
+}
+
+export default TestComponent;
